371e1d055a8e47cfc1b3d3c342b4fa64e34da139
Edomondo <edghimakoll@gmail.com>
2025-09-18 15:40:28 +0100

    feat: add Community Involvement section with dynamic content; update base template and views

--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from website.models import ProfessionalExperience
+from website.models import CommunityInvolvement, ProfessionalExperience
 
 
 def website(request):
     context = {
-        "experiences": ProfessionalExperience.objects.all().order_by("-start_date")
+        "experiences": ProfessionalExperience.objects.all().order_by("-start_date"),
+        "communities": CommunityInvolvement.objects.all().order_by("-start_year"),
     }
 
     return render(request, "website/base.html", context)
