5f4089565e802175b262b68c2eef39bf5818247b
Edomondo <edghimakoll@gmail.com>
2025-09-18 22:36:36 +0100

    feat(publications): create Publications section with dynamic content; update base template and views

--- website/views.py
+++ website/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
-from website.models import CommunityInvolvement, ProfessionalExperience
+from website.models import CommunityInvolvement, Content, ProfessionalExperience
 
 
 def website(request):
     context = {
         "experiences": ProfessionalExperience.objects.all().order_by("-start_date"),
         "communities": CommunityInvolvement.objects.all().order_by("-start_year"),
+        "publications": {
+            "presentations": Content.objects.filter(type="Presentations").order_by(
+                "-published_at"
+            ),
+            "articles": Content.objects.filter(type="Articles").order_by("-published_at"),
+        },
     }
 
     return render(request, "website/base.html", context)
